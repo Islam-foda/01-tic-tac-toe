410e603c4dca60a3f78ab205865674362570163f
islam-foda <eslam.foda@hotmail.com>
2024-09-05 16:41:24 +0300

    add wining marker , add reset button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { useState } from "react";
-
+let btnwinner = document.getElementsByClassName("square");
 
 function Square({ value, onSquareClick }) {
-
   return (
     <>
-      <button
-        onClick={onSquareClick}
-        className="square" >
+      <button onClick={onSquareClick} className="square">
         {value}
       </button>
     </>
@@ -15,30 +12,35 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsnext] = useState(true)
-
-
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsnext] = useState(true);
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
-      return
+      return;
     }
-    const nextSquare = squares.slice()
-    xIsNext ? nextSquare[i] = 'X' : nextSquare[i] = 'O'
-    setSquares(nextSquare)
-    setXIsnext(!xIsNext)
+    const nextSquare =  squares.slice()
+    xIsNext ? (nextSquare[i] = "X") : (nextSquare[i] = "O");
+    setSquares(nextSquare);
+    setXIsnext(!xIsNext);
   }
+function resetGame() {
+  setSquares(Array(9).fill(null))
+status = `Next Player is: "${setXIsnext(!xIsNext)}`;
+for (const btn of btnwinner) {
+  btn.classList.remove("winele")
+}
+  console.log(btnwinner)
+}
 
-  const winner = calculateWinner(squares)
+
+  const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = `Winner: ${winner} `
+    status = `Winner: ${winner} `;
   } else {
-    status = `Next Player is: "${xIsNext?"X":"O"}"`
+    status = `Next Player is: "${xIsNext ? "X" : "O"}"`;
   }
-
-
 
   return (
     <>
@@ -58,10 +60,12 @@ export default function Board() {
         <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
         <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
       </div>
+      <button onClick={resetGame} className="resetbtn" >
+        Reset
+      </button>
     </>
   );
 }
-
 
 function calculateWinner(squares) {
   const lines = [
@@ -72,13 +76,16 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      btnwinner[a].classList.add("winele");
+      btnwinner[b].classList.add("winele");
+      btnwinner[c].classList.add("winele");
       return squares[a];
     }
   }
-  return null
+  return null;
 }
